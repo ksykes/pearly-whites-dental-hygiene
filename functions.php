180b7dd6fc76d46ff637c458835a45c4dcbcbb83
Kait <sykeskaitlyn@gmail.com>
2020-11-19 22:10:59 -0500

    initial commit

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+/* Black Walnut child theme functions and definitions */
+
+// Include the parent theme style.css
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
