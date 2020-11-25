2d9f8f2673a423ad636093a55b63fe816aeedde9
Kait <sykeskaitlyn@gmail.com>
2020-11-25 15:55:43 -0500

    removed unneeded parent page templates

--- functions.php
+++ functions.php
@@ -7,3 +7,18 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+// Remove unneeded parent theme page templates
+
+function child_remove_page_templates( $page_templates ) {
+    unset( $page_templates['page-templates/centered-page.php'] );
+    unset( $page_templates['page-templates/contact-page.php'] );
+    unset( $page_templates['page-templates/front-page-one.php'] );
+    unset( $page_templates['page-templates/front-page-two.php'] );
+    unset( $page_templates['page-templates/menu-page.php'] );
+    unset( $page_templates['page-templates/testimonials-page.php'] );
+
+    return $page_templates;
+}
+
+add_filter( 'theme_page_templates', 'child_remove_page_templates' );
