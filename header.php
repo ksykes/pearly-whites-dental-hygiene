e7f404a76ed6ae4f02efe734948efbc3a40f7648
Kait <sykeskaitlyn@gmail.com>
2020-12-10 18:42:01 -0500

    add alt text to site logo

--- header.php
+++ header.php
@@ -40,7 +40,7 @@
 
 				<?php if ( get_header_image() ) : ?>
 				<div class="title-logo">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt=""></a>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php bloginfo( 'name' ); ?>"></a>
 				</div><!-- end .title-logo -->
 				<?php endif; ?>
 
